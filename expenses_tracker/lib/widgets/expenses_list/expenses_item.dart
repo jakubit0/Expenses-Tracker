c966a28d6edf341d5c3782debfbaa0a1dbb37f62
jakubit0 <kubaleszczyniak@gmail.com>
2023-04-24 16:11:24 +0200

    updated expense item

--- expenses_tracker/lib/widgets/expenses_list/expenses_item.dart
+++ expenses_tracker/lib/widgets/expenses_list/expenses_item.dart
@@ -17,6 +17,14 @@ class ExpenseItem extends StatelessWidget {
             Row(
               children: [
                 Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                )
               ],
             )
           ],
