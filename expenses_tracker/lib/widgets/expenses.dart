a2fb47a97ac78bb3c65cdc876c05bb5c72b01519
jakubit0 <kubaleszczyniak@gmail.com>
2023-04-24 18:54:22 +0200

    created new pop up page

--- expenses_tracker/lib/widgets/expenses.dart
+++ expenses_tracker/lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expenses_tracker/models/expense.dart';
+import 'package:expenses_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,23 +11,42 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-
   final List<Expense> _registerdExpenses = [
-    Expense(title: 'Flutter Course', amount: 19.99, date: DateTime.now(), category: Category.work),
-    Expense(title: 'Cinema', amount: 15.69, date: DateTime.now(), category: Category.leisure),
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context, 
+      builder: (ctx) => const NewExpense(),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children:  [
-           const  Text('the chart'),
-            Expanded(
-              child: ExpensesList(expenses: _registerdExpenses)
-              ),
-        ]
-        ),
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(children: [
+        const Text('the chart'),
+        Expanded(child: ExpensesList(expenses: _registerdExpenses)),
+      ]),
     );
   }
 }
