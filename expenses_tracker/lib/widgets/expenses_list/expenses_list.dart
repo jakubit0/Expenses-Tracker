d616629f604bec9134db3533c28e3f93d0f69438
jakubit0 <kubaleszczyniak@gmail.com>
2023-05-06 19:33:46 +0200

    updated adding new item validation

--- expenses_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expenses_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -9,8 +9,13 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: expenses.length,
-        itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
-        );
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
+    );
   }
 }
--- expenses_tracker/lib/widgets/new_expense.dart
+++ expenses_tracker/lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:expenses_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -31,7 +33,34 @@ class _NewExpenseState extends State<NewExpense> {
   void _submitExpenseData() {
     final enterdAmount = double.tryParse(_amountController.text);
     final amountIsInvalid = enterdAmount == null || enterdAmount <= 0;
-    if (_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null) {}
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Invalid Input'),
+                content: const Text(
+                    'Please make sure a valid title, amount, date and category was entered'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(ctx);
+                    },
+                    child: const Text('Okay'),
+                  )
+                ],
+              ));
+      return;
+    }
+    widget.onAddExpense(
+      Expense(
+          title: _titleController.text,
+          amount: enterdAmount,
+          date: _selectedDate!,
+          category: _selectedCategory),
+    );
+    Navigator.pop(context);
   }
 
   @override
@@ -44,7 +73,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16,16),
       child: Column(
         children: [
           TextField(
